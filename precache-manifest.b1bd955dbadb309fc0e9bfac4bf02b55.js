98236c68ac96f72bf14db27a816d711124997dc6
Arturpak <38945421+Arturpak@users.noreply.github.com>
2020-08-26 12:23:59 +0300

    Updates

---
+++ precache-manifest.b1bd955dbadb309fc0e9bfac4bf02b55.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143e08cc56a56d639447fd8608e66795",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ee1703cccd75023dd26",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "b3cdece31822dff509d1",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6ee1703cccd75023dd26",
+    "url": "/static/js/2.c5c06874.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.c5c06874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cdece31822dff509d1",
+    "url": "/static/js/main.d28ce5bf.chunk.js"
+  },
+  {
+    "revision": "b3913c30f47e82f69392",
+    "url": "/static/js/runtime-main.91def025.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
